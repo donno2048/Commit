16abb1ba5edd8ce444639729263bd6a32b8eafe7
donno2048 <just4now666666@gmail.com>
2021-01-31 17:37:22 +0300

    DONE

--- commit.py
+++ commit.py
@@ -1,1 +1,23 @@
-d428d1ee-63d1-11eb-9138-1cbfc0cc0100
+import git
+from datetime import date, datetime, time, timedelta
+from os import environ, getcwd, system
+from os.path import join
+from random import randint
+from uuid import uuid1
+def create(days, file_name, code, commit_message, final_message, empty_repo_url):
+    repo, file_path = git.Repo.init(getcwd()), join(getcwd(), file_name)
+    for commit_date in [datetime.combine(d, time(hour=randint(0, 23), minute=randint(0, 59), second=randint(0, 59), microsecond=randint(0, 999999))) for d in dates(days)]:
+        open(file_path, "w").write(str(uuid1()))
+        repo.index.add([file_path])
+        environ["GIT_AUTHOR_DATE"] = environ["GIT_COMMITTER_DATE"] = commit_date.strftime("%Y-%m-%d %H:%M:%S")
+        repo.index.commit(commit_message)
+    open(file_path, "w").write(code)
+    environ["GIT_AUTHOR_DATE"] = environ["GIT_COMMITTER_DATE"] = ""
+    repo.index.add([file_path])
+    repo.index.commit(final_message)
+    repo.create_remote("origin", empty_repo_url)
+    system("git push origin master") # repo.remotes.origin.push() Isn't working
+def dates(days):
+    for day_delta in range(days):
+        for i in range(randint(1, 10)): yield date.today() - timedelta(days=day_delta)
+create(1000, "commit.py", open(__file__).read(), "Commit", "DONE", "https://github.com/donno2048/Commit.git")
